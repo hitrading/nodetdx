06fa0ee9af5e5e49e580b3d3eeba0b9828ae294e
iamweilee <383523223@qq.com>
2021-02-22 15:05:15 +0800

    加入host，暂停更新

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const constant = require('./constant');
+const marketHosts = require('./config/hosts');
 const TdxMarketApi = require('./hq');
 const { enableDebug } = require('./log');
 
 module.exports = {
   ...constant,
+  marketHosts,
   enableDebug,
   TdxMarketApi
 };
